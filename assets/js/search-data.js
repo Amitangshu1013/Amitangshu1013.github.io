5bc5ecf910b896ae2d90b885ab44ea0ff085ed81
Amitangshu1013 <mukher44@purdue.edu>
2025-12-01 02:35:09 +0000

    Deploying to gh-pages from @ Amitangshu1013/Amitangshu1013.github.io@fb687f72d109027c5e3a0d953a37aec1b85f63ac 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -25,7 +25,7 @@ ninja.data = [{
           },
         },{id: "nav-cv",
           title: "cv",
-          description: "This is a description of the page. You can modify it in &#39;_pages/cv.md&#39;. You can also change or remove the top pdf download button.",
+          description: "This is my resume.",
           section: "Navigation",
           handler: () => {
             window.location.href = "/cv/";
@@ -437,7 +437,7 @@ ninja.data = [{
         title: 'CV',
         section: 'Socials',
         handler: () => {
-          window.open("/assets/pdf/example_pdf.pdf", "_blank");
+          window.open("/assets/pdf/Amitangshu_Mukherjee_Resume.pdf", "_blank");
         },
       },{
         id: 'social-email',
